52ff80f91cd5b78f81c017517a776f6da84bad34
chunxue.jiao <chunxue.jiao@mall-ai.com>
2020-08-14 19:40:08 +0800

    update

---
+++ js/interview/10.import/index.js
@@ -0,0 +1,10 @@
+import * as data from './module'
+console.log(data);
+// {
+//   default:function default(), name:'Lydia'
+// }
+
+// es6的语法 导入
+// import * as name 语法，我们将module.js 文件中所有export 导入到index.js文件中，并且创建了一个名为data的新对象
+// 在modul.js文件中，有两个导出，默认导出和命名导出。
+// data对象具有默认导出的default属性，其它属性具有指定exports的名称及其对应的值
