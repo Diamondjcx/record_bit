18e8ca42f748092dccd54891f25f76c9c474d362
chunxue.jiao <chunxue.jiao@mall-ai.com>
2020-08-25 20:07:42 +0800

    update

--- js/js机制/3.this/1.js
+++ js/js机制/3.this/1.js
@@ -5,25 +5,25 @@
 // 函数调用方式、传递参数等。this就是这个记录中的一个属性，会在函数执行的过程中用到
 
 function baz() {
-    // 当前调用栈是： baz
-    // 调用位置是全局作用域
+  // 当前调用栈是： baz
+  // 调用位置是全局作用域
 
-    console.log('baz');
-    bar(); // bar的调用位置
+  console.log('baz');
+  bar(); // bar的调用位置
 }
 
 function bar() {
-    // 当前调用栈是baz-》bar
-    // 调用位置在bar中
+  // 当前调用栈是baz-》bar
+  // 调用位置在bar中
 
-    console.log('bar');
-    foo();// foo的调用位置
+  console.log('bar');
+  foo(); // foo的调用位置
 }
 
 function foo() {
-    // 当前调用栈是 baz-》bar-》foo
-    // 调用位置在bar中
-    console.log('foo');
+  // 当前调用栈是 baz-》bar-》foo
+  // 调用位置在bar中
+  console.log('foo');
 }
 baz(); // baz的调用位置
 
@@ -34,7 +34,7 @@ baz(); // baz的调用位置
 
 // 1、默认绑定
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 var a = 2;
 foo(); // 2
@@ -43,21 +43,21 @@ foo(); // 2
 
 // 特例 在严格模式下，foo()不影响默认绑定
 function foo() {
-    'use strict';
-    console.log(this.a);
+  'use strict';
+  console.log(this.a);
 }
-var a= 2;
+var a = 2;
 foo(); // undefined
 
 
 
 // 2、隐式绑定
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 var obj = {
-    a: 2,
-    foo: foo
+  a: 2,
+  foo: foo
 }
 obj.foo(); // 2
 
@@ -65,16 +65,16 @@ obj.foo(); // 2
 // 当函数引用有上下文对象时，隐式绑定规则会把函数调用中的this绑定到这个上下文对象， this.a和obj.a是一样的。
 
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 var obj2 = {
-    a: 42,
-    foo: foo
+  a: 42,
+  foo: foo
 }
 
 var obj1 = {
-    a: 2,
-    obj2: obj2
+  a: 2,
+  obj2: obj2
 }
 
 obj1.obj2.foo() // 42
@@ -82,11 +82,11 @@ obj1.obj2.foo() // 42
 
 //隐式丢失
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 var obj = {
-    a: 2,
-    foo: foo
+  a: 2,
+  foo: foo
 }
 var bar = obj.foo;
 var a = "oops, global";
@@ -96,14 +96,15 @@ bar(); //"oops, global"
 
 // 传入回调函数中
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
+
 function doFoo(fn) {
-    fn();
+  fn();
 }
 var obj = {
-    a: 2,
-    foo: foo
+  a: 2,
+  foo: foo
 }
 var a = 'oops, global'
 
@@ -112,46 +113,47 @@ doFoo(obj.foo); // "oops, global"
 
 // 传入内置回调函数中
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
+
 function doFoo(fn) {
-    fn();
+  fn();
 }
 var obj = {
-    a: 2,
-    foo: foo
+  a: 2,
+  foo: foo
 }
 var a = 'oops, global'
 
 setTimeout(obj.foo, 100); // "oops, global"
 
-function setTimeout(fn, timeout){
-    fn() // 调用位置
+function setTimeout(fn, timeout) {
+  fn() // 调用位置
 }
 
 
 // 3、显示绑定
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 var obj = {
-    a: 2
+  a: 2
 }
 foo.call(obj) // 2 
 // 调用foo时强制把它绑定到obj上
 
 // 3.1硬绑定
 function foo() {
-    console.log(this.a);
+  console.log(this.a);
 }
 
 var obj = {
-    a: 2
+  a: 2
 }
-var bar = function() {
-    foo.call(obj) // 在这里强制绑定硬绑定之后，无论如何调用，foo都会在obj中
+var bar = function () {
+  foo.call(obj) // 在这里强制绑定硬绑定之后，无论如何调用，foo都会在obj中
 }
-bar ();
+bar();
 setTimeout(bar, 100);
 
 bar.call(window)
@@ -160,14 +162,14 @@ bar.call(window)
 /**** */
 
 function foo(something) {
-    console.log(this.a, something);
-    return this.a + something
+  console.log(this.a, something);
+  return this.a + something
 }
 var obj = {
-    a: 2
+  a: 2
 }
-var bar = function() {
-    return foo.apply(obj, arguments)
+var bar = function () {
+  return foo.apply(obj, arguments)
 }
 var b = bar(3); // 2,3
 console.log(b); // 5
@@ -175,11 +177,11 @@ console.log(b); // 5
 
 // bind
 function foo(something) {
-    console.log(this.a, something);
-    return this.a + something
+  console.log(this.a, something);
+  return this.a + something
 }
 var obj = {
-    a: 2
+  a: 2
 }
 var bar = foo.bind(3); // 2,3
 var b = bar(3);
@@ -190,15 +192,15 @@ console.log(b); // 5
 // API调用上下文
 
 function foo(el) {
-    console.log(el, this.id);
+  console.log(el, this.id);
 }
 var obj = {
-    id: 'awesome'
+  id: 'awesome'
 }
 // 调用foo(...)时把this绑定到obj
-[1,2,3].forEach(foo, obj)
+[1, 2, 3].forEach(foo, obj)
 // 1 awesome 2 awesome 3 awesome
-这些函数实际上通过call()或者apply 实现了显示绑定，少写代码。
+这些函数实际上通过call() 或者apply 实现了显示绑定， 少写代码。
 
 
 // 4 new绑定
@@ -211,9 +213,90 @@ var obj = {
 // 4、如果函数没有其他对象，那么new表达式中的函数调用会自动返回这个新对象
 
 function foo(a) {
-    this.a = a;
+  this.a = a;
 }
 var bar = new foo(2)
 console.log(bar.a); // 2
 // 使用new来调用foo(...)时，会构造一个新对象并把它绑定到foo(...)调用中的this上。
 
+
+// 判断this
+// 1. 函数是否在 new 中调用（new 绑定）？如果是的话 this 绑定的是新创建的对象。 
+// var bar = new foo() 
+// 2. 函数是否通过 call、apply（显式绑定）或者硬绑定调用？
+// 如果是的话，this 绑定的是 指定的对象。 var bar = foo.call(obj2) 
+// 3. 函数是否在某个上下文对象中调用（隐式绑定）？
+// 如果是的话，this 绑定的是那个上 下文对象。 var bar = obj1.foo() 
+// 4. 如果都不是的话，使用默认绑定。如果在严格模式下，就绑定到 undefined，否则绑定到 全局对象。 
+// var bar = foo()
+
+
+
+// 被忽略的this
+function foo() {
+  console.log(this.a);
+}
+var a = 2;
+foo.call(null); // 2
+
+
+
+function foo(a, b) {
+  console.log("a:" + a + ", b:" + b);
+}
+// 把数组“展开”成参数 
+foo.apply(null, [2, 3]); // a:2, b:3
+// 使用 bind(..) 进行柯里化
+var bar = foo.bind(null, 2);
+bar(3); // a: 2 b: 3
+
+
+// 箭头函数 不使用上面四种规则，而是根据外层（函数或者作用域）来决定this
+function foo() {
+  // 返回一个箭头函数
+  return (a) => {
+    console.log(this.a);
+  }
+}
+var obj1 = {
+  a: 2
+}
+var obj2 = {
+  a: 3
+}
+var bar = foo.call(obj1)
+bar.call(obj2); // 2 不是3
+
+// foo() 内部创建的箭头函数会捕获调用时 foo() 的 this。
+// 由于 foo() 的 this 绑定到 obj1， 
+// bar（引用箭头函数）的 this 也会绑定到 obj1，
+// 箭头函数的绑定无法被修改。（new 也不 行！）
+
+
+// 箭头函数最常用于回调函数中，例如事件处理器和定时器
+function foo() {
+  setTimeout((a) => {
+    console.log(this.a);
+  }, 200);
+}
+
+var obj = {
+  a: 2
+}
+
+foo.call(obj) // 2
+
+
+// 等同于
+
+function foo() {
+  var self = this;
+  setTimeout(function () {
+    console.log(self.a);
+  }, 200);
+}
+
+var obj = {
+  a: 2
+}
+foo.call(obj) // 2
--- js/引用类型/Object类型.md
+++ js/引用类型/Object类型.md
@@ -19,3 +19,92 @@ for (const [key, value] of Object.entries(object1)) {
 
 
 console.log(`${(x=>x)('I love')} china`);
+
+#### 字面量转换为对象
+```
+var strPrimitive = "I am a string"; 
+console.log( strPrimitive.length ); // 13 
+console.log( strPrimitive.charAt( 3 ) ); // "m"
+
+直接在字符串字面量上访问属性或方法。引擎自动把字面量转换成String对象，所以可以访问属性和方法。
+
+42.359.toFixed(2)
+引擎会把42转换成new Number(42) 
+```
+```
+var myObject = {
+  a:2
+};
+myObject.a //2 属性访问
+myObject['a'] //2 键访问 
+实际上是同一个位置，返回相同的值2
+```
+#### 注
+- 在对象中，属性名永远都是字符串。如果你使用 string（字面量）以外的其他值作为属性 名，那它首先会被转换为一个字符串。即使是数字也不例外，虽然在数组下标中使用的的 确是数字，但是在对象属性名中数字会被转换成字符串，所以当心不要搞混对象和数组中 数字的用法：
+```
+var myObject = { }; 
+myObject[true] = "foo"; 
+myObject[3] = "bar"; 
+myObject[myObject] = "baz"; 
+myObject["true"]; // "foo" 
+myObject["3"]; // "bar" 
+myObject["[object Object]"]; // "baz"
+```
+
+#### 复制
+- 浅复制
+```
+var newObj = JSON.parse( JSON.stringify( someObj ) );
+
+var newObj = Object.assign( {}, myObject ); 
+newObj.a; // 2 
+newObj.b === anotherObject; // true 
+newObj.c === anotherArray; // true 
+newObj.d === anotherFunction; // true
+它会遍历一个或多个源对象的所有可枚举的自有键，并把它们复制(使用=操作符赋值)到目标对象，最后返回目标对象
+
+```
+
+##### [[Get]]
+- 属性访问
+```
+var myObject = {
+  a: 2
+}
+myObject.a // 2
+
+myObject.a是一次属性访问。实际上是实现了[[Get]]操作 (有点像函数调用：[[Get]]() )
+对象默认的内置[[Get]]操作首先在对象中查找是否有名称相同的属性
+如果找到就会返回这个属性的值
+如果没有找到名称相同的属性，按照[[Get]]算法的定义会执行另外一种非常重要的行为
+遍历可能存在的[[Prototype]]原型链
+如果没有找到名称相同的属性，那[[Get]]操作会返回值undefined
+var myObject = {
+  a: 2
+}
+myObject.b // 2
+```
+- 和访问变量时是不一样的。如果引用了一个当前词法作用域中不存在的变量，并不会像对象属性一样返回undefined，会抛出一个ReferenceError异常
+```
+var myObject = 
+{ 
+  a: undefined
+};
+myObject.a; // undefined 
+myObject.b; // undefined
+
+返回值的角度来说，这两个引用没有区别，都返回了undefined，底层的[[Get]]操作对myObject.b进行了更复杂的处理。
+
+```
+- 如何区分
+```
+var myObject = { 
+  a: undefined
+};
+("a" in myObject) // true
+("b" in myObject) // false
+myObject.hasOwnProperty("a") // true
+myObject.hasOwnProperty("b") // false
+```
+  - in操作符会检查属性是否在对象及其[[Prototype]]原型链中
+  - hasOwnProperty()只会检查属性是否在myObject对象中，不会检查原型链
